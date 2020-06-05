988cf8fae04e0fcceac9c4d13c9deb19b63a8eb4
mabiddlecomb <59177525+mabiddlecomb@users.noreply.github.com>
2020-06-04 23:27:43 -0400

    Add files via upload

---
+++ sitejs.js
@@ -0,0 +1,8 @@
+function submit(){
+    var fName = document.getElementById("fName").value;
+    var lName = document.getElementById("lname").value;
+    var company = document.getElementById("company").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var desc = document.getElementById("desc").value;
+}
